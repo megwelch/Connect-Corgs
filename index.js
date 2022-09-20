2df34787017a035c53c06430428b1e04796ec92a
megwelch <meghanmwelch@gmail.com>
2022-09-20 11:47:16 -0400

    js not working to place chip

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const columns = document.getElementsByClassName("column")
 const columnsArr = Array.from(columns)
+const winCombos = [
+    [0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5], 
+    [6, 7, 8, 9], [7, 8, 9, 10], [8, 9, 10, 11],
+    [12, 13, 14, 15], [13, 14, 15, 16], [14, 15, 16, 17],
+    [18, 19, 20, 21], [19, 20, 21, 22], [20, 21, 22, 23],
+    [24, 25, 26, 27], [25, 26, 27, 28], [26, 27, 28, 29],
+    [30, 31, 32, 33], [31, 32, 33, 34], [32, 33, 34, 35],
+    [36, 37, 38, 39], [37, 38, 39, 40], [38, 39, 40, 41],
+    [0, 6, 12, 18], [1, 7, 13, 19], [2, 8, 14, 20],
+    [3, 9, 15, 21], [4, 10, 16, 22], [5, 11, 17, 23],
+    [6, 12, 18, 24], [7, 13, 9, 25], [8, 14, 20, 26],
+    [9, 15, 21, 27], [10, 16, 22, 28], [11, 17, 23, 29],
+    [12, 18, 24, 30], [13, 19, 25, 31], [14, 20, 26, 32],
+    [15, 21, 27, 33], [16, 22, 28, 34], [17, 23, 29, 35],
+    [18, 24, 30, 36], [19, 25, 31, 37], [20, 26, 32, 38],
+    [21, 27, 33, 39], [22, 28, 34, 40], [23, 29, 35, 41],
+    [2, 9, 16, 23,], [1, 8, 15, 22], [8, 15, 22, 29],
+    [0, 7, 14, 21], [7, 14, 21, 28], [14, 21, 28, 35],
+    [6, 13, 20, 27], [13, 20, 27, 34], [20, 27, 34, 41],
+    [12, 19, 26, 33], [19, 26, 33, 40], [18, 25, 32, 39],
+    [3, 8, 13, 18], [4, 9, 14, 19], [9, 14, 19, 24],
+    [5, 10, 15, 20], [10, 15, 20, 25], [15, 20, 25, 30],
+    [11, 16, 21, 26], [16, 21, 26, 31], [21, 26, 31, 36],
+    [17, 22, 27, 32], [22, 27, 32, 37], [23, 28, 33, 38]
+]
 
 const startGame = () => {
-    columnsArr.forEach(column => column.addEventListener('click', playSpot))
+    columnsArr.forEach(column => column.addEventListener('click', playSlot))
 }
 
-const playSpot = (event) => {
-    const playedSpot = event.target.getAttribute('class')
-    console.log(playedSpot, 'click')
+const playSlot = (event) => {
+    const playedSlot = event.target.getAttribute('id')
+    console.log(playedSlot)
+    let column = event.target.parentElement
+    console.log(column)
+    console.log(column.lastChild.innerHTML)
+    if (column.lastChild.innerHTML === ''){
+        column.lastChild.classList.add('snoots')
+    }
 }
 
 startGame()
